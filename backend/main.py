53f2aa642d0b6ba830fe530202b128fab7fb3e99
Jaleel-VS <jeremikress@gmail.com>
2023-12-31 17:37:46 +0200

    Add new origin for localhost:5173

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,9 @@ origins = [
     "http://localhost:4100",
     "http://127.0.0.1:5500",
     "http://127.0.0.1:8000",
+    # 5173
+    "http://localhost:5173",
+    
     
     # Add more origins as needed
 ]
